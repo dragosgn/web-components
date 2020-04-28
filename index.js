9f10cc5c227bfb565b3e09e871526c91e2b5dd3c
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-04-28 17:44:58 +0200

    slots

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ template.innerHTML = `
        <div>
         <h3></h3>
         <div class="info">
-            <p>EMAIL</p>
-            <p>PHONE</p>
+            <p><slot name="email" /></p>
+            <p><slot name="phone" /></p>
         </div>
         <button id="toggle-info">Hide Info</button>
        </div>
