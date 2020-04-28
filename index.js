59223b3a7d08f928aa56a0917d1f50b7f29fdfcf
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-04-28 17:39:26 +0200

    added some nice css

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const template = document.createElement("template");
 template.innerHTML = `
-    <style>h3 { color: coral }</style>
+    <style>
+    .user-card {
+        font-family: 'Arial', sans-serif;
+        background: #f4f4f4;
+        width: 500px;
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+        grid-gap: 10px;
+        margin-bottom: 15px;
+        border-bottom: darkorchid 5px solid;
+    }
+
+    .user-card img {
+        width: 100%;
+    }
+
+    .user-card button {
+        cursor: pointer;
+        background: darkorchid;
+        color: #fff;
+        border: 0;
+        border-radius: 5px;
+        padding: 5px 10px;
+    }
+    </style>
     <div class="user-card">
     <img />
-       <div><h3></h3></div>
+       <div>
+        <h3></h3>
+        <div class="info">
+            <p>EMAIL</p>
+            <p>PHONE</p>
+        </div>
+        <button id="toggle-info">Hide Info</button>
+       </div>
     </div>
 `;
 
